152ae241af0a99122ea54d3d3a5227d98684807b
Charles <rubiacharles164@gmail.com>
2026-07-28 13:35:02 -0230

    pom and applications to send the code into the db

---
+++ src/main/java/QAP4Application.java
@@ -0,0 +1,13 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class QAP4Application {
+    public static void main(String[] args) {
+        SpringApplication.run(QAP4Application.class, args);
+    }
+}
